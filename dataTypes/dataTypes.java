e3dc659708a523869ed6abfed251f738605847e1
exceps <sefametin68@gmail.com>
2024-01-30 13:05:09 +0300

    helloWorld, variables, dataTypes

---
+++ dataTypes/dataTypes.java
@@ -0,0 +1,27 @@
+package dataTypes;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        /*  
+            boolean = true, false
+            char = single character
+            byte = integer 1 byte (-+128)
+            short = integer 2 bytes (-+32768)
+            int = integer 4 bytes (-+2000000000)
+            long = integer 8 bytes
+            float = floating-point number  4 bytes
+            double = floating-point number 8 bytes
+         */
+        
+        int sayi;
+        sayi= 13;
+
+        char karakter = 's';
+        
+        String sehir = "ankara";
+
+        boolean dogruMu = false;
+
+
+    }
+}
