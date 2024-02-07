f98c64dabe09e0e623d6d4b56e3d48d2a123c0b8
sefametin <sefametin68@gmail.com>
2024-02-07 16:27:14 +0300

    wq

--- classes/main.java
+++ classes/main.java
@@ -1,6 +1,6 @@
 package classes;
 
-public class main {
+public class Main {
     public static void main(String[] args){
         //referance type
         CustomerManager customerManager = new CustomerManager();
