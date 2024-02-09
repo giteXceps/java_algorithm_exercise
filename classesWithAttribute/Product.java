bbace78aeed97233ab7f060a294d175dd0167a10
sefametin <sefametin68@gmail.com>
2024-02-09 23:07:40 +0300

    getter setter

--- classesWithAttribute/Product.java
+++ classesWithAttribute/Product.java
@@ -14,41 +14,53 @@ public class Product {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     public int getStockAmount() {
         return stockAmount;
     }
+
     public void setStockAmount(int stockAmount) {
         this.stockAmount = stockAmount;
     }
+
     public String getColor() {
         return color;
     }
+
     public void setColor(String color) {
         this.color = color;
     }
+
     public String getCode() {
-        code = this.name.substring(0,1)+this.id;
+        code = this.name.substring(0, 1) + this.id;
         return code;
     }
 }
