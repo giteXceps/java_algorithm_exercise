037aae55e11c2f859d48702b9b2045482614b4d3
sefametin <sefametin68@gmail.com>
2024-02-18 00:44:37 +0300

    interfaceDemo

---
+++ interfaceDemo/Robot.java
@@ -0,0 +1,15 @@
+package interfaceDemo;
+
+public class Robot implements IWorkable, IMaintables{
+
+    @Override
+    public void work() {
+        
+    }
+
+    @Override
+    public void maintance() {
+        
+    }
+
+}
