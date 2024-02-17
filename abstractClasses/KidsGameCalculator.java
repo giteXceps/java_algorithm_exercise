201d3a70e935ebfa35b636e632fd269384c54545
sefametin <sefametin68@gmail.com>
2024-02-18 00:09:48 +0300

    abstractClasses, abstractDemo

---
+++ abstractClasses/KidsGameCalculator.java
@@ -0,0 +1,10 @@
+package abstractClasses;
+
+public class KidsGameCalculator extends GameCalculator{
+
+    @Override
+    public void calculate() {
+        System.out.println("Your Score = 100");
+    }
+    
+}
---
+++ abstractDemo/MySqlDatabaseManager.java
@@ -0,0 +1,10 @@
+package abstractDemo;
+
+public class MySqlDatabaseManager extends BaseDatabaseManager{
+
+    public void getData() {
+        System.out.println("Getting data from MYSQL");
+    }
+
+}
+
