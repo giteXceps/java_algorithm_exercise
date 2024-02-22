32cfc4872dd10b18d08cf3697669e090e587a220
sefametin <sefametin68@gmail.com>
2024-02-22 22:00:15 +0300

    wq

--- helloWorld/javaHelloWorld.java
+++ helloWorld/javaHelloWorld.java
@@ -1,9 +1,10 @@
 package helloWorld;
+
 public class javaHelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Merhaba JAVA");
         System.out.println("Merhaba JAVA 2");
     }
-    
+
 }
