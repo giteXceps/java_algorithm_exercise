87962be2ebc325e654acff31bf9b7b9d45e11da0
sefametin <sefametin68@gmail.com>
2024-03-28 00:27:38 +0300

    generics

---
+++ repositoryGenericsDemo/src/ProductDal.java
@@ -0,0 +1,21 @@
+public class ProductDal implements IEntityRepository<Product>{
+
+    @Override
+    public void add(Product entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void delete(Product entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public void update(Product entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+}
